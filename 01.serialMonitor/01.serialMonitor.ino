d27735eed3b036e760b5e6d48ec1ff5340cd9c9d
TheSpiciestRamen <159206642+TheSpiciestRamen@users.noreply.github.com>
2024-08-14 12:51:14 +1000

    Finished stuff on data storage for lesson, and started pin data readings on the serial monitor.

--- 01.serialMonitor/01.serialMonitor.ino
+++ 01.serialMonitor/01.serialMonitor.ino
@@ -1,5 +1,6 @@
 /*
-  Author: 
+  Author: Axel Clark 
+  14/8/2024
   
   Learning Intention:
   The students will understand the difference between the inbuilt setup and loop methods,
@@ -21,12 +22,16 @@
 
 // The setup function runs once when you press reset or power the board
 void setup() {
-
+  Serial.begin(9600);
+  Serial.print("Serial Monitor Debug");
 }
 
 // The loop function runs over and over again forever
 void loop() {
-
+  Serial.print("Pin-A0:");
+  Serial.print(analogRead(A0));
+  Serial.print(",");
+  Serial.print
 }
 
 
